56820ad1444beaabf99016d9759e71dc16811588
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2023-11-06 01:43:15 +0600

    11-06-2023

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -81,7 +81,7 @@
                                                 alt="IMG-PRODUCT">
 
                                             <a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04"
-                                                href="images/product-detail-01.jpg') }}">
+                                                href="{{ asset('frontend/images/product-detail-01.jpg') }}">
                                                 <i class="fa fa-expand"></i>
                                             </a>
                                         </div>
@@ -100,13 +100,13 @@
                                         </div>
                                     </div>
 
-                                    <div class="item-slick3" data-thumb="images/product-detail-03.jpg') }}">
+                                    <div class="item-slick3" data-thumb="{{ asset('frontend/images/product-detail-03.jpg') }}">
                                         <div class="wrap-pic-w pos-relative">
                                             <img src="{{ asset('frontend/images/product-detail-03.jpg') }}"
                                                 alt="IMG-PRODUCT">
 
                                             <a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04"
-                                                href="images/product-detail-03.jpg') }}">
+                                                href="{{ asset('frontend/images/product-detail-03.jpg') }}">
                                                 <i class="fa fa-expand"></i>
                                             </a>
                                         </div>
