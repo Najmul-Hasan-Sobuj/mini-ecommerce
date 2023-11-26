50cb513838d7e24f7b2c119c39225d9cdfd7c05d
Najmul-Hasan-Sobuj <najmulhasansobuj87@gmail.com>
2023-11-27 00:34:23 +0600

    11-27-2023

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -278,86 +278,97 @@
                         <div class="tab-pane fade" id="reviews" role="tabpanel">
                             <div class="row">
                                 <div class="col-sm-10 col-md-8 col-lg-6 m-lr-auto">
-                                    <div class="p-b-30 m-lr-15-sm">
-                                        <!-- Review -->
-                                        <div class="flex-w flex-t p-b-68">
-                                            <div class="wrap-pic-s size-109 bor0 of-hidden m-r-18 m-t-6">
-                                                <img src="{{ asset('frontend/images/avatar-01.jpg') }}"
-                                                    alt="AVATAR">
+                                    @auth
+                                        <div class="p-b-30 m-lr-15-sm">
+                                            <!-- Review -->
+                                            <div class="flex-w flex-t p-b-68">
+                                                <div class="wrap-pic-s size-109 bor0 of-hidden m-r-18 m-t-6">
+                                                    <img src="{{ asset('frontend/images/avatar-01.jpg') }}"
+                                                        alt="AVATAR">
+                                                </div>
+
+                                                <div class="size-207">
+                                                    <div class="flex-w flex-sb-m p-b-17">
+                                                        <span class="mtext-107 cl2 p-r-20">
+                                                            Ariana Grande
+                                                        </span>
+
+                                                        <span class="fs-18 cl11">
+                                                            <i class="zmdi zmdi-star"></i>
+                                                            <i class="zmdi zmdi-star"></i>
+                                                            <i class="zmdi zmdi-star"></i>
+                                                            <i class="zmdi zmdi-star"></i>
+                                                            <i class="zmdi zmdi-star-half"></i>
+                                                        </span>
+                                                    </div>
+
+                                                    <p class="stext-102 cl6">
+                                                        Quod autem in homine praestantissimum atque optimum est, id
+                                                        deseruit. Apud ceteros autem philosophos
+                                                    </p>
+                                                </div>
                                             </div>
 
-                                            <div class="size-207">
-                                                <div class="flex-w flex-sb-m p-b-17">
-                                                    <span class="mtext-107 cl2 p-r-20">
-                                                        Ariana Grande
-                                                    </span>
-
-                                                    <span class="fs-18 cl11">
-                                                        <i class="zmdi zmdi-star"></i>
-                                                        <i class="zmdi zmdi-star"></i>
-                                                        <i class="zmdi zmdi-star"></i>
-                                                        <i class="zmdi zmdi-star"></i>
-                                                        <i class="zmdi zmdi-star-half"></i>
-                                                    </span>
-                                                </div>
+                                            <!-- Add review -->
+                                            <form class="w-full">
+                                                <h5 class="mtext-108 cl2 p-b-7">
+                                                    Add a review
+                                                </h5>
 
                                                 <p class="stext-102 cl6">
-                                                    Quod autem in homine praestantissimum atque optimum est, id
-                                                    deseruit. Apud ceteros autem philosophos
+                                                    Your email address will not be published. Required fields are marked *
                                                 </p>
-                                            </div>
+
+                                                <!-- Assuming product_id and user_id are handled in the backend or with hidden fields -->
+                                                <input type="hidden" name="product_id" value="your_product_id">
+                                                <input type="hidden" name="user_id" value="your_user_id">
+
+                                                <div class="flex-w flex-m p-t-50 p-b-23">
+                                                    <span class="stext-102 cl3 m-r-16">
+                                                        Your Rating
+                                                    </span>
+
+                                                    <span class="wrap-rating fs-18 cl11 pointer">
+                                                        <i class="item-rating pointer zmdi zmdi-star-outline"></i>
+                                                        <i class="item-rating pointer zmdi zmdi-star-outline"></i>
+                                                        <i class="item-rating pointer zmdi zmdi-star-outline"></i>
+                                                        <i class="item-rating pointer zmdi zmdi-star-outline"></i>
+                                                        <i class="item-rating pointer zmdi zmdi-star-outline"></i>
+                                                        <input class="dis-none" type="number" name="rating">
+                                                    </span>
+                                                </div>
+
+                                                <div class="row p-b-25">
+                                                    <div class="col-12 p-b-5">
+                                                        <label class="stext-102 cl3" for="review">Your review</label>
+                                                        <textarea class="size-110 bor8 stext-102 cl2 p-lr-20 p-tb-10" id="review" name="review"></textarea>
+                                                    </div>
+
+                                                    <div class="col-sm-6 p-b-5">
+                                                        <label class="stext-102 cl3" for="name">Name</label>
+                                                        <input class="size-111 bor8 stext-102 cl2 p-lr-20" id="name"
+                                                            type="text" name="name">
+                                                    </div>
+
+                                                    <div class="col-sm-6 p-b-5">
+                                                        <label class="stext-102 cl3" for="email">Email</label>
+                                                        <input class="size-111 bor8 stext-102 cl2 p-lr-20" id="email"
+                                                            type="text" name="email">
+                                                    </div>
+                                                </div>
+
+                                                <button
+                                                    class="flex-c-m stext-101 cl0 size-112 bg7 bor11 hov-btn3 p-lr-15 trans-04 m-b-10">
+                                                    Submit
+                                                </button>
+                                            </form>
                                         </div>
-
-                                        <!-- Add review -->
-                                        <form class="w-full">
-                                            <h5 class="mtext-108 cl2 p-b-7">
-                                                Add a review
-                                            </h5>
-
-                                            <p class="stext-102 cl6">
-                                                Your email address will not be published. Required fields are marked *
-                                            </p>
-
-                                            <div class="flex-w flex-m p-t-50 p-b-23">
-                                                <span class="stext-102 cl3 m-r-16">
-                                                    Your Rating
-                                                </span>
-
-                                                <span class="wrap-rating fs-18 cl11 pointer">
-                                                    <i class="item-rating pointer zmdi zmdi-star-outline"></i>
-                                                    <i class="item-rating pointer zmdi zmdi-star-outline"></i>
-                                                    <i class="item-rating pointer zmdi zmdi-star-outline"></i>
-                                                    <i class="item-rating pointer zmdi zmdi-star-outline"></i>
-                                                    <i class="item-rating pointer zmdi zmdi-star-outline"></i>
-                                                    <input class="dis-none" type="number" name="rating">
-                                                </span>
-                                            </div>
-
-                                            <div class="row p-b-25">
-                                                <div class="col-12 p-b-5">
-                                                    <label class="stext-102 cl3" for="review">Your review</label>
-                                                    <textarea class="size-110 bor8 stext-102 cl2 p-lr-20 p-tb-10" id="review" name="review"></textarea>
-                                                </div>
-
-                                                <div class="col-sm-6 p-b-5">
-                                                    <label class="stext-102 cl3" for="name">Name</label>
-                                                    <input class="size-111 bor8 stext-102 cl2 p-lr-20" id="name"
-                                                        type="text" name="name">
-                                                </div>
-
-                                                <div class="col-sm-6 p-b-5">
-                                                    <label class="stext-102 cl3" for="email">Email</label>
-                                                    <input class="size-111 bor8 stext-102 cl2 p-lr-20" id="email"
-                                                        type="text" name="email">
-                                                </div>
-                                            </div>
-
-                                            <button
-                                                class="flex-c-m stext-101 cl0 size-112 bg7 bor11 hov-btn3 p-lr-15 trans-04 m-b-10">
-                                                Submit
-                                            </button>
-                                        </form>
-                                    </div>
+                                    @endauth
+                                    @guest
+                                        <p class="stext-102 cl6 text-center">
+                                            Please <a href="{{ route('login') }}">log in</a> to submit a review.
+                                        </p>
+                                    @endguest
                                 </div>
                             </div>
                         </div>
